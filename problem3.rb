d06cfe8f092800f32f0295953ba33b43eacc1931
Chris Bloom <chrisbloom7@gmail.com>
2010-03-21 22:30:39 -0400

    Problems 1 - 3

---
+++ problem3.rb
@@ -0,0 +1,14 @@
+# The prime factors of 13195 are 5, 7, 13 and 29.
+# 
+# What is the largest prime factor of the number 600851475143 ?
+
+def factor_primes n
+  a=1;b=1
+  while a < n do
+    a=a+1
+    next if not n.modulo(a).zero? or a == b
+    b=b*a
+    puts "a=#{a};b=#{b}"
+    break if b == n
+  end
+end
